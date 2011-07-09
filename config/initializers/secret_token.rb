cfcd53059205755c181f5086a266077f429c2618
Yann Mainier <yann.mainier@gmail.com>
2011-07-09 23:29:14 +0200

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tsl::Application.config.secret_token = 'f38aa1f65416f34c3a40cb2b6054cdaa3dc7df050d1be9757f88ad6341b34b628ed457e3711ca573ddac490666fa2bf7c2250dbe31e342d2e9a20c941169b76c'
